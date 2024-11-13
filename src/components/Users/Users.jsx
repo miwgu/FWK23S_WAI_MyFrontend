d848794cdc44474eb3b8897e6f1b3abc39d77ca0
Miwa G <miwa.guhres@hotmail.com>
2024-11-13 10:42:22 +0100

    change to table and add css

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDataFetcher } from '../Data/DataFetcherProvider';
 import UsersList from "./UsersList";
+import styles from './Users.module.css';
 
 const Users = () => {
       const { getAllUsers, error } = useDataFetcher();
@@ -20,24 +21,16 @@ const Users = () => {
       if (error) return <div>Error: {error}</div>;
 
       return (
-          <div>
-              <h2>All Users</h2>
-              <UsersList data={users} />
+          <div className={styles.container} >
+              <h2 className={styles.titleContent}>
+                All Users
+              </h2>
+              <div className={styles.content}>
+                <UsersList data={users} />
+              </div>
           </div>
       );
     };
-    
-    /* const styles = {
-      container: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-      },
-      text: {
-        fontSize: '24px',
-        fontWeight: 'bold',
-      },
-    }; */
+  
 
 export default Users
--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
+import styles from './UsersList.module.css';
 
 const UsersList = ({ data }) => {
     if (!data || data.length === 0) return <div>No users found!</div>;
+    console.log("all users: ", data)
 
     return (
-        <div>
-            <ul>
-                {data.map(user => (
-                    <li key={user.id}>
-                        <p>Id: {user.id}</p>
-                        <img src={user.avatar} alt={`${user.username}'s avatar`} width={50} height={50} />
-                        <p>Email: {user.email}</p>
-                        <p>Username: {user.username}</p>
-                        <p>Role: {user.role}</p>
-                    </li>
-                ))}
-            </ul>
+        <div className={styles.tableContainer}>
+            <table className={styles.usersTable}>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Avatar</th>
+                        <th>Email</th>
+                        <th>Username</th>
+                        <th>Role</th>
+                        <th>Created at</th>
+                        <th>Updated at</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(user => (
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>
+                                <img src={user.avatar} alt={`${user.username}'s avatar`} width={50} height={50} />
+                            </td>
+                            <td>{user.email}</td>
+                            <td>{user.username}</td>
+                            <td>{user.role}</td>
+                            <td>{user.created_at}</td>
+                            <td>{user.updated_at}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
